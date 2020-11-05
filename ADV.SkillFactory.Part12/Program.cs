ff6127a6a2a23c35bf4314fd87ac087eecb7ddb0
Дмитрий Афанасьев <a1d1@inbox.ru>
2020-11-06 01:26:16 +0800

    Рефакторинг кода, удалены коментарии

--- ADV.SkillFactory.Part12/Program.cs
+++ ADV.SkillFactory.Part12/Program.cs
@@ -18,45 +18,12 @@ namespace ADV.SkillFactory.Part12
         /// <param name="args">Аргументы</param>
         static void Main(string[] args)
         {
-            //#region Cycles
-
-            //var listBackgroundColor = Enum.GetValues(typeof(ConsoleColor));
-            //string helloWord = "Hello World!!!!!";
-
-            //do
-            //{
-            //    int i = 0;
-            //    foreach (ConsoleColor color in listBackgroundColor)
-            //    {
-            //        char letter = helloWord[i];
-
-            //        if (color == ConsoleColor.Black)
-            //        {
-            //            Console.ForegroundColor = ConsoleColor.White;
-            //        }
-            //        else
-            //        {
-            //            Console.ForegroundColor = color;
-            //        }
-
-            //        Console.Write(letter);
-            //        Thread.Sleep(100);
-            //        i++;
-            //    }
-
-            //    i = 0;
-
-            //    Console.WriteLine(" ");
-            //    Thread.Sleep(2000);
-
-            //} while (true);
-
-            //#endregion
+           
 
             #region Array
 
             string[] brick = new string[3];
-            for (int i = 0; i < brick.Length;  i= i +1)
+            for (int i = 0; i < brick.Length;  i= i +1)       
             {
                 brick[i] = "Кирпич " + i;
             }
