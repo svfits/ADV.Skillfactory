b6635e5610ce50cd76ab58a68680170d82f50a87
Дмитрий Афанасьев <a1d1@inbox.ru>
2020-12-05 21:20:56 +0800

    Раскраска по номерам

--- SkillFactory.ColoringNumbers/Program.cs
+++ SkillFactory.ColoringNumbers/Program.cs
@@ -15,19 +15,20 @@ namespace SkillFactory.ColoringNumbers
 
             var str = new[] { HelloWorldEn, HelloWorldRus, HelloWorldEs };
             var list = Enum.GetValues(typeof(ConsoleColor)).Cast<ConsoleColor>().ToArray();
-
+            
             do
             {
                 foreach (var strs in str)
                 {
                     foreach (var word in strs)
                     {
-                        Console.Write(word);
                         var rnd = random.Next(list.Count());
                         Console.ForegroundColor = list[rnd];
 
+                        Console.Write(word);
                         System.Threading.Thread.Sleep(150);
                     }
+                
                     Console.WriteLine();
                 }
             } while (true);
